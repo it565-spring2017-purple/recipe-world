0e839e7c55aef7de2f3b49830c4d38dc67732d70
ravaliakkati <ravali.akkati@gmail.com>
2017-03-28 09:25:22 -0500

    Added profile icons

--- recipeworld/AppDelegate.swift
+++ recipeworld/AppDelegate.swift
@@ -17,6 +17,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        UINavigationBar.appearance().barTintColor = UIColor(red: 0.8, green: 0, blue: 0, alpha: 1.0)
+        
         return true
     }
 
